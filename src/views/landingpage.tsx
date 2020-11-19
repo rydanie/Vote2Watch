fd6eff5380de4e0bb6dd55baa22c83daa1a938d1
Noah <59939241+noahkonen@users.noreply.github.com>
2020-11-18 19:58:35 -0500

    Landing Page Update

--- src/views/landingpage.tsx
+++ src/views/landingpage.tsx
@@ -1,29 +1,85 @@
-import React from 'react';
-import { Container, Button } from 'reactstrap';
+import React, {Component} from 'react';
+import { Button } from 'reactstrap';
 
-const Example = (props: any) => {
+
+class App extends Component {
+  
+  render() {
+    
+    
+    return(
+
+      
+      <div className="App">
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <a href="createserverpage">
+        <Button color="primary" active={true} size="lg">Create a Room</Button>
+        </a>
+        <br /><br />
+        <a href="joinroompage">
+        <Button color="secondary" active={true} size="lg">Join a Room</Button>
+        </a>
+
+      </div>
+
+      
+    )
+    
+  }
+  
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+/*const Example = (props: any) => {
   return (
 
+
+
+
+    
     <Container style={{flex:1,
+      width: 500px;
+      margin: auto;
       flexDirection:'row',
       alignItems:'center',
       justifyContent:'center',
       }}>
 
-  
-    
-    <div text-align="center"> 
+
+<>
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+  <Button variant="primary" size="lg" color='primary' block>
+    Create a Room
+  </Button>
+  <Button variant="secondary" size="lg" block>
+    Join a Room
+  </Button>
+</>
+     
       <div text-align="center">
         <a href="createserverpage">
           <Button size="lg" color="primary">Create A Room</Button>
         </a>
       </div>
 
-      <div>
+      <div text-align="center">
         <a href="joinroompage">
           <Button  size="lg" color="primary">Join A Room</Button>
+
         </a>
-      </div>
+      
     </div>
 
     <a href="secondpage"> go to test page</a>
@@ -34,4 +90,5 @@ const Example = (props: any) => {
   );
 }
 
-export default Example;
+
+export default Example;*/
