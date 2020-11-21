327668c95106733ab66ebec0d8dcb0d151c6283d
ethanb123 <ethanbowman9@gmail.com>
2020-11-21 18:09:30 -0500

    Made nested if statements inline with && operator. Cleaned up code and comments.

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -5,28 +5,24 @@ import CookieService from './cookieservice';
 // Array storing all the movie objects in the room
 let ArrayOfMovies: any[] = [];
 
-// Main page element. Maps out movies from the database into the CreateRow Function
 const Results = (props: any) => {
   
   // Maps out all movies in the database
   Database.movies.map((data, key) => {
       
-      // Checks that there are movies are in database
-      if (Database.movies.length > 1) {
-
-          // Checks that the movie is in the same roomID as the users cookie. 
-          // Pushes movie objects into ArrayOfMovies and sorts by votes
-          if (data.movieRoomID.toString() === CookieService.get("RoomID")){
-            ArrayOfMovies.push(data)
-            ArrayOfMovies.sort((a, b) => (b.votes > a.votes)? 1 : -1)
-            console.log(ArrayOfMovies)
-          }
-      }
+    // Checks that the movie is in the same roomID as the users cookie. 
+    // Pushes movie objects into ArrayOfMovies and sorts by votes
+    if (data.movieRoomID.toString() === CookieService.get("RoomID") && Database.movies.length > 1){
+      ArrayOfMovies.push(data)
+      ArrayOfMovies.sort((a, b) => (b.votes > a.votes)? 1 : -1)
+      console.log(ArrayOfMovies)
+    }
+    return <div />
   })
 
   // Returns results if there was a two way tie
   if(ArrayOfMovies[0]?.votes === ArrayOfMovies[1]?.votes){
-    return (<h2>{ArrayOfMovies[0]?.movieName} is tied with {ArrayOfMovies[2]?.movieName} as winner with {ArrayOfMovies[0]?.votes} votes</h2>)
+    return (<h2>{ArrayOfMovies[0]?.movieName} is tied with {ArrayOfMovies[1]?.movieName} as winner with {ArrayOfMovies[0]?.votes} votes</h2>)
   }
   
   // Returns results if there is a single winner
--- src/views/landingpage.tsx
+++ src/views/landingpage.tsx
@@ -1,45 +1,30 @@
 import React, {Component} from 'react';
 import { Button } from 'reactstrap';
 
-
 class App extends Component {
   
   render() {
-    
-    
     return(
-
-      
       <div className="App">
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <a href="createroompage">
-        <Button color="primary" active={true} size="lg">Create a Room</Button>
+          <Button color="primary" active={true} size="lg">Create a Room</Button>
         </a>
+
         <br /><br />
+
         <a href="joinroompage">
-        <Button color="secondary" active={true} size="lg">Join a Room</Button>
+          <Button color="secondary" active={true} size="lg">Join a Room</Button>
         </a>
-
+        
       </div>
-
-      
     )
-    
   }
-  
+
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
 
 
 /*const Example = (props: any) => {
